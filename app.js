9480c295c861ec097f4439f144dab4d46c3a018e
Mohit <mohit1909.mk@gmail.com>
2022-06-07 16:30:02 +0530

    commit envs

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.DB_URL;
+//'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
