df7f532cbbcd42f90c6215c7dcfb216ea71b1330
Mohit <mohit1909.mk@gmail.com>
2022-06-15 15:27:23 +0530

    complete

--- app.js
+++ app.js
@@ -22,12 +22,15 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+
 // const mongoSanitize = require('express-mongo-sanitize');
 
 
-// const dbUrl = process.env.DB_URL;
-//'mongodb://localhost:27017/yelp-camp'
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.dbUrl || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, { 
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -51,10 +54,22 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(mongoSanitize());
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24*60*60 
+})
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR",e)
+})
 
 const sessionConfig = {
+    store,
     name:'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -153,4 +168,4 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`LISTENING ON PORT NUMBER ${port}`);
-})
+}) 
